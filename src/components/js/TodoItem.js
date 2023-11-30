bbce2568f2663c6831c985b73da717b2fec63c72
jihoon_test <wlgns130587@naver.com>
2023-11-30 14:56:29 +0900

    awsProject

---
+++ src/components/js/TodoItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../css/TodoItem.css';
+ 
+class TodoItem extends React.Component {
+ 
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.props.isComplete !== nextProps.isComplete;
+    }
+ 
+    render() {
+        const { content, isComplete, id, onToggle, onRemove } = this.props;
+ 
+        return (
+            <div className="todo-item" onClick={() => onToggle(id)}>
+                <div className="todo-item-remove" onClick={(e) => {
+                    e.stopPropagation();    // onToggle 이 실행되지 않도록 함
+                    onRemove(id)
+                }
+                }>
+                    &times;
+                </div>
+                <div className={`todo-item-text ${isComplete && 'isComplete'}`}>
+                    <div>
+                        {content}
+                    </div>
+                </div>
+                {
+                    isComplete && (<div className="isComplete-mark">✓</div>)
+                }
+            </div>
+        )
+    }
+}
+ 
+export default TodoItem;
